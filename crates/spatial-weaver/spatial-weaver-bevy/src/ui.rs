75e3696dd96dbd7df485f45626660b76f60889ce
CorneliusCorn <jrosenbeckerdev@gmail.com>
2024-04-30 20:18:07 -0500

    working on bevy interaction plugin

--- crates/spatial-weaver/spatial-weaver-bevy/src/ui.rs
+++ crates/spatial-weaver/spatial-weaver-bevy/src/ui.rs
@@ -1,13 +1,17 @@
-use std::process::Command;
-
 use bevy::{
 	ecs::{
 		component::Component,
+		entity::Entity,
+		event::EventReader,
 		system::{Query, Res},
 	},
 	math::Vec3,
 	time::Time,
 	transform::components::Transform,
+};
+use bevy_rapier3d::{
+	pipeline::{CollisionEvent, ContactForceEvent},
+	plugin::RapierContext,
 };
 
 use crate::Interactor3D;
@@ -21,10 +25,24 @@ pub struct BevyButton3D {
 	collider_bounds: Vec3,
 }
 
-pub fn check_3D_interactions(
-	interactors: Query<(&Transform, &Interactor3D)>,
-	mut elements: Query<(&Transform, &mut BevyButton3D)>,
-	time: Res<Time>,
+fn check_ui_intersections(
+	pointer: Query<(Entity, &Interactor3D)>,
+	rapier_context: Res<RapierContext>,
 ) {
-	for (transform, mut button) in elements.iter() {}
+	// TODO: change this later, we're going to support multiple pointers, I'm just lazy
+	let entity = pointer
+		.get_single()
+		.expect("FIXME: multiple pointers detected in scene");
+
+	/* Iterate through all the intersection pairs involving a specific collider. */
+	for (collider1, collider2, intersecting) in
+		rapier_context.intersection_pairs_with(entity.0)
+	{
+		if intersecting {
+			println!(
+				"The entities {:?} and {:?} have intersecting colliders!",
+				collider1, collider2
+			);
+		}
+	}
 }
